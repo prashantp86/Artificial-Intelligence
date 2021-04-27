6b32f4b32d6fdf2352bf0e0d19b154718a56fcd3
Aman kumar <45587567+aman-ku@users.noreply.github.com>
2021-04-27 11:14:21 +0530

    Update mrf_denoising_ans.m

--- Markov Random Field/mrf_denoising_ans.m
+++ Markov Random Field/mrf_denoising_ans.m
@@ -20,7 +20,7 @@ max_iter = 1000;
 lamda = 5;
 mu = .1;
 
-for epochs=1:max_iter
+for epochs=1:max_iter:
     E1 = sum((new_image-Image).^2);
     E2 = sum(sum(diff(new_image,11).^2))+sum(sum(diff(new_image,1,2).^2));
     d1 = new_image-Image; 
